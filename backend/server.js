df02c36934206eabd1fd556c81342581ac148c46
kenziemcallister <mackenziemcallister64@gmail.com>
2026-05-04 11:30:47 -0500

    Link to localhost

--- backend/server.js
+++ backend/server.js
@@ -8,5 +8,5 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
