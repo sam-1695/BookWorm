803649acccd1b78382dd6b7470d51f8703cc4da1
sam1695 <sam1695.dev@gmail.com>
2026-05-07 19:56:43 -0500

    explore page finished

--- src/app/pages/explore/explore.ts
+++ src/app/pages/explore/explore.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../../book/book-service';
+import { Book } from '../../book/book-interface';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-explore',
@@ -6,4 +9,38 @@ import { Component } from '@angular/core';
   templateUrl: './explore.html',
   styleUrl: './explore.css',
 })
-export class Explore {}
+export class Explore implements OnInit {
+  featuredBook: Book | null = null;
+  editorsPicks$: Observable<Book[]>;
+  newReleases$: Observable<Book[]>;
+  trending$: Observable<Book[]>;
+
+  constructor(private bookService: BookService) {
+    const books$ = this.bookService.getBookUpdateListener();
+
+    this.editorsPicks$ = books$.pipe(
+      map(books => this.shuffle(books).slice(0, 5))
+    );
+
+    this.newReleases$ = books$.pipe(
+      map(books => [...books].reverse().slice(0, 5))
+    );
+
+    this.trending$ = books$.pipe(
+      map(books => this.shuffle(books).slice(0, 5))
+    );
+  }
+
+  ngOnInit(): void {
+    this.bookService.getPosts();
+    this.bookService.getBookUpdateListener().subscribe(books => {
+      if (books.length > 0 && !this.featuredBook) {
+        this.featuredBook = books[0];
+      }
+    });
+  }
+
+  private shuffle(array: any[]) {
+    return array.sort(() => Math.random() - 0.5);
+  }
+}
